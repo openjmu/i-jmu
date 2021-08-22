55d0b9947547741a2b88253cc370e8c704093dc8
Alex Li <github@alexv525.com>
2021-08-22 23:27:33 +0800

    🎉 Initial commit

---
+++ android/app/src/main/kotlin/com/openjmu/iJMU/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.openjmu.iJMU
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (
+                (!isTaskRoot
+                        && intent != null
+                        && intent.hasCategory(Intent.CATEGORY_LAUNCHER)
+                        && intent.action != null
+                        && intent.action == Intent.ACTION_MAIN) ||
+                intent.flags and Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT != 0
+        ) {
+            finish()
+            return
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.i("OpenJMU", "MainActivity - onActivityResult")
+    }
+}
