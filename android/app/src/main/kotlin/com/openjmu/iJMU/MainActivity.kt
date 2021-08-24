3c05f2bc4681f27599dfe51379cf6ddcd6d43a6c
Alex Li <github@alexv525.com>
2021-08-24 22:35:47 +0800

    💄 Transparent activity

--- android/app/src/main/kotlin/com/openjmu/iJMU/MainActivity.kt
+++ android/app/src/main/kotlin/com/openjmu/iJMU/MainActivity.kt
@@ -3,6 +3,7 @@ package com.openjmu.iJMU
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
@@ -24,5 +25,14 @@ class MainActivity: FlutterActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         Log.i("OpenJMU", "MainActivity - onActivityResult")
+    }
+
+    override fun onPostResume() {
+        super.onPostResume()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.apply {
+            navigationBarColor = 0
+            statusBarColor = 0
+        }
     }
 }
