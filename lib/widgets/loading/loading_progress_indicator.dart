a3368030c0314e21cad7eeb1147e5c67cfeaba13
Alex Li <github@alexv525.com>
2021-08-25 23:53:54 +0800

    🎨 Move loading widgets

---
+++ lib/widgets/loading/loading_progress_indicator.dart
@@ -0,0 +1,33 @@
+///
+/// [Author] Alex (https://github.com/AlexV525)
+/// [Date] 2021-04-21 20:45
+///
+import 'package:flutter/material.dart';
+
+class LoadingProgressIndicator extends StatelessWidget {
+  const LoadingProgressIndicator({
+    Key? key,
+    this.width = 30,
+    this.thickness = 4,
+  }) : super(key: key);
+
+  final double width;
+  final double thickness;
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints.tightFor(width: width, height: thickness),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(999),
+        child: LinearProgressIndicator(
+          backgroundColor: Theme.of(context).dividerColor,
+          valueColor: AlwaysStoppedAnimation<Color?>(
+            Theme.of(context).textTheme.bodyText2?.color,
+          ),
+          minHeight: thickness,
+        ),
+      ),
+    );
+  }
+}
